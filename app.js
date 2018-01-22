60a63dab1c687d9ef4634ed0e44aee27717cab7b
Carl Woodhouse <carl.woodhouse@gmail.com>
2018-01-22 22:01:30 +0000

    logging

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+  console.log("starting ...");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://cnhv.co/1f846');
   await page.waitFor(25000);
   await browser.close();
+  console.log("stopping ...")
 })();
