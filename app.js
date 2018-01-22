9c4d57509e5a7f3ad1a7999521fb92e751e68fd9
Carl Woodhouse <carl.woodhouse@gmail.com>
2018-01-22 22:20:27 +0000

    wrapping in express

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 8080;
 
-(async () => {
-  console.log("starting ...");
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://cnhv.co/1f846');
-  await page.waitFor(25000);
-  await browser.close();
-  console.log("stopping ...")
-})();
+app.get('/', function(req, res) {
+    (async () => {
+        console.log("starting ...");
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://cnhv.co/1f91s');
+        await page.waitFor(60000);
+        await browser.close();
+        console.log("stopping ...")
+      })();
+
+    // ejs render automatically looks in the views folder
+    res.send('Running ...s');
+});
+
+
+app.listen(port, function() {
+    console.log('Our app is running on port: ' + port);
+});
